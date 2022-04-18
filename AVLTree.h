1ab83cc971633ea0b2492bbcde3e4e2c59e93132
Rudy Lucas <rrlucas@live.com>
2022-04-17 20:54:23 -0500

    modifying AVLTree; testing attributes

--- AVLTree.h
+++ AVLTree.h
@@ -48,6 +48,10 @@ public:
     value& insertNode(const key& kData) {
         privateInsertNode(kData, rootPtr);  // return privateInsertNode
     }
+    int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+    // calculate the height
     int getHeight(const AVLNode<key, value> *&aNode) {
         if (aNode == nullptr) {     // if there is no node, then the height is -1
             return -1;
@@ -56,9 +60,55 @@ public:
             return aNode->height;       // returns the height of the node
         }
     }
-    
+    // create a new AVLNode
+    AVLNode<key, value> *newNode(key kData, value vData) {
+        AVLNode<key, value> *newNode = new AVLNode<key, value>;       // allocate memory
+        newNode->keyData = kData;          // insert data into keyData pointer
+        newNode->leftPtr = nullptr;        // left pointer will be null
+        newNode->rightPtr = nullptr;       // right pointer will be null
+        newNode->height = 1;               // height of the new node is now 1
+        return newNode;
+    }
+    // rotate right
+    AVLNode<key, value> *rotateRight(AVLNode<key, value> *rotateR) {
+        AVLNode<key, value> *xPtr = rotateR->leftPtr;
+        AVLNode<key, value> *yPtr = xPtr->rightPtr;
+        xPtr->rightPtr = rotateR;
+        rotateR->leftPtr = yPtr;
+        rotateR->height = max(getHeight(rotateR->leftPtr), getHeight(rotateR->rightPtr)) + 1;
+        xPtr->height = max(getHeight(xPtr->leftPtr), getHeight(xPtr->rightPtr)) + 1;
+        return xPtr;
+    }
+    // rotate left
+    AVLNode<key, value> *rotateLeft(AVLNode<key, value> *rotateL) {
+        AVLNode<key, value> *xPtr = rotateL->rightPtr;
+        AVLNode<key, value> *yPtr = xPtr->leftPtr;
+        xPtr->leftPtr = rotateL;
+        rotateL->rightPtr = yPtr;
+        rotateL->height = max(getHeight(rotateL->leftPtr), getHeight(rotateL->rightPtr)) + 1;
+        xPtr->height = max(getHeight(yPtr->leftPtr), getHeight(yPtr->rightPtr) + 1);
+        return xPtr;
+    }
+    // get the balance factor of each node (difference between the height of the left subtree minus right subtree)
+    int getBalanceFactor(AVLNode<key, value> *checkNode) {
+        if (checkNode == nullptr) {
+            return 0;
+        }
+        return getHeight(checkNode->leftPtr) - getHeight(checkNode->rightPtr);
+    }
 
-
+    // perform inorder traversal
+    void inOrderTraversal() {
+        printInOrder(rootPtr);
+    }
+    // perform inorder traversal of AVLTree
+    void printInOrder(AVLNode<key, value> *ptr) {
+        if (ptr != nullptr) {
+            printInOrder(ptr->leftPtr);
+            cout << ptr->valData << " ";
+            printInOrder(ptr->rightPtr);
+        }
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include "DocumentParser.h"
+#include "AVLTree.h"
 
 int main() {
-    string word = "baked";
-    Porter2Stemmer::stem(word);
-    cout << word << endl << endl;
+//    string word = "baked";
+//    Porter2Stemmer::stem(word);
+//    cout << word << endl << endl;
 
 
-    DocumentParser file("blogs_0000001.json");
+    // DocumentParser file("blogs_0000001.json");
+    AVLTree<int, int> intTree;
+    cout << "Enter 10 integers: \n";
+
+    // insert 10 integers to AVLTree
+    for (int i{0}; i < 10; ++i) {
+        int value{0};
+        cin >> value;
+        intTree.insertNode(value);
+    }
+    intTree.inOrderTraversal();
 
 
     return 0;
